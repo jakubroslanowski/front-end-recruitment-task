270b5e585eedd3e16cb152c7e307cc602bd4b099
Jakub Rosłanowski <jakub.roslanowski@gmail.com>
2021-02-10 23:14:47 +0100

    Add third task

--- level-3/index.php
+++ level-3/index.php
@@ -3,35 +3,17 @@
 include_once 'vendor/autoload.php';
 
 $content = [
-    'title' => 'Welcome to your Frontend developer recruitment task!',
-    'box' => [
-        'title' => 'Ultrices dui sapien eget mi',
-        'content' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet cursus sit amet dictum sit. Augue ut lectus arcu bibendum at varius vel. Urna neque viverra justo nec ultrices dui sapien. Nunc sed id semper risus. Condimentum vitae sapien pellentesque habitant morbi tristique senectus et netus. Sit amet facilisis magna etiam tempor orci. Ipsum suspendisse ultrices gravida dictum fusce ut placerat orci nulla. Bibendum arcu vitae elementum curabitur vitae nunc sed velit dignissim. Nec nam aliquam sem et. Cursus mattis molestie a iaculis at.',
-        'images_default' => [
-            'large' => [
-                'url' => 'assets/images/sean-o-KMn4VEeEPR8-unsplash_1_s6zmfh_c_scale,w_1400.jpg',
-                'width' => 1400,
-                'height' => 931
-            ],
-            'medium' => [
-                'url' => 'assets/images/sean-o-KMn4VEeEPR8-unsplash_1_s6zmfh_ar_16_9,c_fill,g_auto__c_scale,w_720.jpg',
-                'width' => 720,
-                'height' => 405
-            ],
-            'small' => [
-                'url' => 'assets/images/sean-o-KMn4VEeEPR8-unsplash_1_s6zmfh_c_scale,w_480.jpg',
-                'width' => 480,
-                'height' => 319
-            ],
-        ],
-        'images' => array_map(function ($src) {
-            list($width, $height) = getimagesize($src);
-            return [
-                'url' => $src,
-                'width' => $width,
-                'height' => $height
-            ];
-        }, glob("assets/images/*.jpg"))
+    'title' => 'Lorem Ipsum',
+    'text' => 'Lorem ipsum dolor sit amet, consectetur adipis scing elit. Pellentesque ac
+    nisi felis. Suspendisse ut finibus augue.',
+    'button' => 'Button',
+    'img' => 'assets/images/sean-o-KMn4VEeEPR8-unsplash_1_s6zmfh_c_scale,w_1400.jpg',
+    'tooltip' => 'Some tooltip text!',
+    'modal' => [
+      'title' => 'Super modal',
+      'text' => 'Lorem Ipsum',
+      'send' => 'Send message',
+      'close' => 'Close'
     ]
 ];
 
